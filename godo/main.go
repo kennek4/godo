09239a81420065f240b59630048ea3b026ecb545
kennek4 <xktabz@gmail.com>
2024-10-01 23:08:48 -0600

    Update: go.sum imports

--- godo/main.go
+++ godo/main.go
@@ -1,7 +1,10 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import "fmt"
+import "github.com/go-do/godo/cmd"
 
 func main() {
-	fmt.Println("Hello World!")
+	cmd.Execute()
 }
