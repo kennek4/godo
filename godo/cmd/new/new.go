2d266f46dc1df5430a14d8a54a206d857f7230cf
kennek4 <xktabz@gmail.com>
2024-10-02 17:14:12 -0600

    Update: added imports

--- godo/cmd/new/new.go
+++ godo/cmd/new/new.go
@@ -7,11 +7,12 @@ import (
 	"fmt"
 
 	"github.com/kennek4/godo/cmd"
+	"github.com/kennek4/godo/cmd/new/subcmds"
 	"github.com/spf13/cobra"
 )
 
 // newCmd represents the new command
-var NewCmd = &cobra.Command{
+var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Creates a new task or task group",
 	Long: `
@@ -23,12 +24,26 @@ will be added to a default task group
 Example:
 	godo new task "Add docstrings" // Creates a task in the default task group
 	godo new task "Create tests for this file" "tests" // Creates a task in the tests task group
+	
 `,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if err := cobra.MaximumNArgs(1); err != nil {
+			return fmt.Errorf("too many arguments given, provide only one argument when using this command")
+		}
+
+		if err := cobra.MinimumNArgs(1); err != nil {
+			return fmt.Errorf("no arguments given, please enter one when using this command")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("new called")
 	},
 }
 
 func init() {
-	cmd.RootCmd.AddCommand(NewCmd)
+	cmd.RootCmd.AddCommand(newCmd)
+	newCmd.AddCommand(subcmds.GroupCmd)
+	newCmd.AddCommand(subcmds.TaskCmd)
 }
