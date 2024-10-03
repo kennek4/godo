74ca037ea78e43562c68954e5f41b388e17d8b13
kennek4 <xktabz@gmail.com>
2024-10-02 23:38:20 -0600

    Update: changed directory name

---
+++ godo/cmd/new/new.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package new
+
+import (
+	"fmt"
+
+	"github.com/kennek4/godo/cmd"
+	"github.com/spf13/cobra"
+)
+
+// newCmd represents the new command
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Creates a new task or task group",
+	Long: `
+
+This command creates either a task or task group.
+If no task group is explicitly given, the task 
+will be added to a default task group
+
+Example:
+	godo new task "Add docstrings" // Creates a task in the default task group
+	godo new task "Create tests for this file" "tests" // Creates a task in the tests task group
+	
+`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if err := cobra.MaximumNArgs(1); err != nil {
+			return fmt.Errorf("too many arguments given, provide only one argument when using this command")
+		}
+
+		if err := cobra.MinimumNArgs(1); err != nil {
+			return fmt.Errorf("no arguments given, please enter one when using this command")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("new called")
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(newCmd)
+}
---
+++ godo/cmd/root.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2024 Ken Tabanay kentabanay@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// The directory location of godo
+	GodoPath string
+
+	// Boolean to see if godo is already initialized
+	IsInitalized bool
+)
+
+// rootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "godo",
+	Short: "A todo list organizer to keep track of tasks in your current working directory",
+	Long:  `A CLI todo list organizer made in Go to keep track of tasks you need to do in your development environment.`,
+}
+
+func printTasks() {
+
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	viper.SetDefault("godoPath", "./.godo")
+	viper.SetDefault("todoFile", "todos.json")
+}
