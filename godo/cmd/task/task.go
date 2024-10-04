6a99551c740ddb48b40e4de3fe28382742f3d75c
kennek4 <xktabz@gmail.com>
2024-10-04 16:49:03 -0600

    Add: Added the command boilerplate

---
+++ godo/cmd/task/task.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package task
+
+import (
+	"fmt"
+
+	"github.com/kennek4/godo/cmd"
+	"github.com/spf13/cobra"
+)
+
+// taskCmd represents the task command
+var taskCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Used for task manipulations",
+	Long: `This task is the base command for task manipulations such as viewing a certain task,
+completing a certain task, editing a task, etc.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("task called")
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(taskCmd)
+}
