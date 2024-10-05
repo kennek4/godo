319a4868ef3b889ea097f2b879bc0ef5a22c2c77
kennek4 <xktabz@gmail.com>
2024-10-05 15:29:25 -0600

    Update: converted if statement to a switch

--- godo/internal/util/prompt.go
+++ godo/internal/util/prompt.go
@@ -7,20 +7,24 @@ import (
 )
 
 // Prompts the user if they would like to proceed by asking Y/n
-func PromptUser(prompt string) (userResponse bool, err error) {
+func PromptUser(prompt *string) (userResponse bool, err error) {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("%s", prompt)
+	fmt.Printf("%s", *prompt)
 	fmt.Print("Would you like to proceed? [Y/n] ")
 
-	r, _, err := reader.ReadRune()
+	char, _, err := reader.ReadRune()
 	if err != nil { // Rune could not be encoded to UTF-8
 		return false, fmt.Errorf("rune could not be encoded to UTF-8")
 	}
 
-	if r == 'Y' || r == 'y' {
+	switch char {
+	case 'Y':
+	case 'y':
 		return true, nil
-	} else if r == 'N' || r == 'n' {
+
+	case 'N':
+	case 'n':
 		return false, nil
 	}
 
