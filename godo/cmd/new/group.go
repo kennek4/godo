49b4b2ba332adff508663659b8bed14511bfe340
kennek4 <xktabz@gmail.com>
2024-10-06 14:55:30 -0600

    Add: new group command functionality

---
+++ godo/cmd/new/group.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package new
+
+import (
+	"fmt"
+
+	"github.com/kennek4/godo/cmd"
+	"github.com/kennek4/godo/internal/util/dbdriver"
+	"github.com/spf13/cobra"
+)
+
+// groupCmd represents the group command
+var groupCmd = &cobra.Command{
+	Use:   "group",
+	Short: "Creates a new task group",
+	Long: `This command creates a new task group for task organization.
+The following command would create a new task group called "Homework"
+> godo new group Homework
+`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		groupName := args[0]
+		if groupName == "" {
+			return fmt.Errorf("can't create a new group with an empty name, please try again")
+		}
+
+		if groupName != "" {
+			err := createGroup(&groupName)
+			if err != nil {
+				return err
+			}
+
+			return nil // Group was successfully created
+		}
+
+		return fmt.Errorf("failed to create a new table")
+	},
+}
+
+func createGroup(groupName *string) error {
+
+	if groupName == nil {
+		err := fmt.Errorf("in createGroup, groupName was supplied a nil string pointer")
+		return err
+	}
+
+	dbdriver.CreateTableInDB(*groupName, &cmd.GodoDir)
+	return nil
+}
+
+func init() {
+	groupCmd.PersistentFlags().StringP("groupName", "n", "", "The name of the group that the user wishes to create")
+	newCmd.AddCommand(groupCmd)
+}
--- godo/internal/util/dbdriver/create_table_in_db.go
+++ godo/internal/util/dbdriver/create_table_in_db.go
@@ -2,23 +2,31 @@ package dbdriver
 
 import "fmt"
 
-func CreateTableInDB(newTableName *string, dbDir *string) error {
-
-	if newTableName == nil {
-		return fmt.Errorf("the given table name argument is nil")
-	}
+func CreateTableInDB(newTableName string, dbDir *string) error {
 
 	db, err := GetDB(dbDir)
 	if err != nil {
 		return err
 	}
 
+	defer db.Close()
+
 	table := fmt.Sprintf(`
 	CREATE TABLE IF NOT EXISTS %s (
 		id INTEGER NOT NULL PRIMARY KEY, 
 		title TEXT NOT NULL,
 		description TEXT,
-		isComplete BOOLEAN NOT NULL DEFAULT FALSE);`)
+		isComplete BOOLEAN NOT NULL DEFAULT FALSE);`, newTableName)
+
+	statement, err := db.Prepare(table)
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec()
+	if err != nil {
+		return err
+	}
 
 	return nil // Table was created
 }
