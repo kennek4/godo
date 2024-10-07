ddfdc8fd07c1ffa2d132ebefbd3a2cab08cec615
kennek4 <xktabz@gmail.com>
2024-10-06 21:57:11 -0600

    Update: Removed db.prepare statement

--- godo/internal/util/dbdriver/create_table_in_db.go
+++ godo/internal/util/dbdriver/create_table_in_db.go
@@ -6,26 +6,21 @@ func CreateTableInDB(newTableName string, dbDir *string) error {
 
 	db, err := GetDB(dbDir)
 	if err != nil {
-		return err
+		return fmt.Errorf("in CreateTableInDB, something went wrong with getting the db")
 	}
 
 	defer db.Close()
 
 	table := fmt.Sprintf(`
-	CREATE TABLE IF NOT EXISTS %s (
-		id INTEGER NOT NULL PRIMARY KEY, 
-		title TEXT NOT NULL,
-		description TEXT,
-		isComplete BOOLEAN NOT NULL DEFAULT FALSE);`, newTableName)
+CREATE TABLE IF NOT EXISTS %s (
+	id INTEGER NOT NULL PRIMARY KEY,
+	title TEXT NOT NULL,
+	description TEXT,
+	isComplete BOOLEAN NOT NULL DEFAULT FALSE);`, newTableName)
 
-	statement, err := db.Prepare(table)
+	_, err = db.Exec(table)
 	if err != nil {
-		return err
-	}
-
-	_, err = statement.Exec()
-	if err != nil {
-		return err
+		return fmt.Errorf("in CreateTableInDB, something went wrong with executing the query")
 	}
 
 	return nil // Table was created
