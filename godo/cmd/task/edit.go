d6b99799b75ca6c4db7081136cdab482269be95a
kennek4 <xktabz@gmail.com>
2024-10-12 16:49:40 -0600

    Add: added boilerplate file for editCmd

---
+++ godo/cmd/task/edit.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package task
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// editCmd represents the edit command
+var editCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		processEditCmd()
+	},
+}
+
+func processEditCmd() {
+
+}
+
+func init() {
+	TaskCmd.AddCommand(editCmd)
+}
